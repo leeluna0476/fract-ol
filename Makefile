a2a6e9f764c177d0d4c2e5de8c9bb71cf72b10c1
leeluna0476 <leeluna0476@daum.net>
2023-12-07 14:25:18 +0900

    implement ft_atof: using atof while parsing julia_c(real, imag). since atof is forbidden function, had to implement my own atof. exceptions are handled before calling ft_atof

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ HEADERS=$(HEADER_DIR)main.h $(HEADER_DIR)ui.h $(HEADER_DIR)fractal.h
 MLX_DIR=./mlx
 MLX_FLAGS=-L$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit
 SRCS_DIR=./srcs/
-SRCS=$(SRCS_DIR)main.c $(SRCS_DIR)main_utils.c $(SRCS_DIR)ui_init_screen.c $(SRCS_DIR)ui_key.c $(SRCS_DIR)ui_mouse.c $(SRCS_DIR)ui_button.c $(SRCS_DIR)ui_menu.c $(SRCS_DIR)fractal_utils.c $(SRCS_DIR)fractal_utils_recurrence.c $(SRCS_DIR)fractal_utils_execute.c $(SRCS_DIR)logistic_utils.c $(SRCS_DIR)mandelbrot.c $(SRCS_DIR)julia.c $(SRCS_DIR)burningship.c $(SRCS_DIR)tricorn.c $(SRCS_DIR)logistic_map.c
+SRCS=$(SRCS_DIR)main.c $(SRCS_DIR)main_utils.c $(SRCS_DIR)main_utils_parse.c $(SRCS_DIR)ui_init_screen.c $(SRCS_DIR)ui_key.c $(SRCS_DIR)ui_mouse.c $(SRCS_DIR)ui_button.c $(SRCS_DIR)ui_menu.c $(SRCS_DIR)fractal_utils.c $(SRCS_DIR)fractal_utils_recurrence.c $(SRCS_DIR)fractal_utils_execute.c $(SRCS_DIR)logistic_utils.c $(SRCS_DIR)mandelbrot.c $(SRCS_DIR)julia.c $(SRCS_DIR)burningship.c $(SRCS_DIR)tricorn.c $(SRCS_DIR)logistic_map.c
 OBJS=$(SRCS:.c=.o)
 
 all: $(TARGET)
--- includes/main.h
+++ includes/main.h
@@ -6,7 +6,7 @@
 /*   By: seojilee <seojilee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 09:29:55 by seojilee          #+#    #+#             */
-/*   Updated: 2023/12/07 13:02:42 by seojilee         ###   ########.fr       */
+/*   Updated: 2023/12/07 14:09:18 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 # include "ui.h"
 
 typedef struct s_julia_parse {
-	int	num;
-	int	neg;
-	int	dot;
+	int		int_num;
+	double	double_num;
+	int		sign;
+	int		dot;
 }		t_julia_parse;
 
 // main_utils
@@ -28,5 +29,10 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *s);
 int		check_if_my_fractal(char *fractal, t_data *img);
 void	call_set_argv(t_data *img);
+
+// main_utils_parse
+bool	ft_isdigit(int c);
+bool	is_num(char *arg);
+double	ft_atof(const char *str);
 
 #endif
