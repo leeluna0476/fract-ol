a77c8c8cde5d429b95408475e0031d38a981ba13
leeluna0476 <leeluna0476@daum.net>
2023-12-07 18:36:59 +0900

    modify menu bar and add theme buttons

--- srcs/ui_mouse.c
+++ srcs/ui_mouse.c
@@ -6,7 +6,7 @@
 /*   By: seojilee <seojilee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 13:47:53 by seojilee          #+#    #+#             */
-/*   Updated: 2023/12/07 16:38:11 by seojilee         ###   ########.fr       */
+/*   Updated: 2023/12/07 18:36:38 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,29 @@ void	init_zoom_center(t_data *img)
 
 void	send_click_to_key(t_data *img, int y)
 {
-	if (y >= 280 && y <= 310)
+	if (y >= 130 && y <= 160)
+		control_key(THEME_RED, img);
+	else if (y >= 180 && y <= 210)
+		control_key(THEME_BLUE, img);
+	else if (y >= 280 && y <= 310)
 		control_key(KEY_0, img);
-	if (y >= 330 && y <= 360)
+	else if (y >= 330 && y <= 360)
 		control_key(KEY_1, img);
-	if (y >= 380 && y <= 410)
+	else if (y >= 380 && y <= 410)
 		control_key(KEY_2, img);
-	if (y >= 430 && y <= 460)
+	else if (y >= 430 && y <= 460)
 		control_key(KEY_3, img);
-	if (y >= 480 && y <= 510)
+	else if (y >= 480 && y <= 510)
 		control_key(KEY_4, img);
-	if (y >= 530 && y <= 560)
+	else if (y >= 530 && y <= 560)
 		control_key(KEY_5, img);
-	if (y >= 580 && y <= 610)
+	else if (y >= 580 && y <= 610)
 		control_key(KEY_6, img);
-	if (y >= 630 && y <= 660)
+	else if (y >= 630 && y <= 660)
 		control_key(KEY_7, img);
-	if (y >= 680 && y <= 710)
+	else if (y >= 680 && y <= 710)
 		control_key(KEY_8, img);
-	if (y >= 730 && y <= 760)
+	else if (y >= 730 && y <= 760)
 		control_key(KEY_9, img);
 }
 
@@ -81,6 +85,12 @@ int	mouse_move(int x, int y, void *param)
 		return (1);
 	img->mouse.x = x;
 	img->mouse.y = y;
+	start_y = 130;
+	while (start_y <= 180)
+	{
+		_button(img, 30, start_y);
+		start_y += 50;
+	}
 	start_y = 280;
 	while (start_y <= 730)
 	{
