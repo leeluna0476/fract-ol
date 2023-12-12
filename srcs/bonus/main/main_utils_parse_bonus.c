e684e175100c322e0825d03bb3e119bafb385e2e
leeluna0476 <leeluna0476@daum.net>
2023-12-12 09:45:59 +0900

    modify parser: abort -- or 0.

--- srcs/bonus/main/main_utils_parse_bonus.c
+++ srcs/bonus/main/main_utils_parse_bonus.c
@@ -6,7 +6,7 @@
 /*   By: seojilee <seojilee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 20:39:25 by seojilee          #+#    #+#             */
-/*   Updated: 2023/12/11 20:39:32 by seojilee         ###   ########.fr       */
+/*   Updated: 2023/12/12 09:44:49 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,31 @@ bool	ft_isdigit(int c)
 	return (false);
 }
 
+void	init_parser(t_julia_parse *count)
+{
+	count->dot = 0;
+	count->sign = 0;
+	count->int_num = 0;
+}
+
 bool	is_num(char *arg)
 {
 	t_julia_parse	count;
 	size_t			i;
 
+	init_parser(&count);
 	i = 0;
-	count.dot = 0;
-	count.sign = 0;
-	count.int_num = 0;
-	while (i < ft_strlen(arg))
+	while (arg[i] && (arg[i] == '+' || arg[i] == '-'))
+	{
+		count.sign++;
+		i++;
+	}
+	while (arg[i])
 	{
 		if (!ft_isdigit(arg[i]))
 		{
 			if (arg[i] == '.')
 				count.dot++;
-			else if (arg[i] == '-' || arg[i] == '+')
-				count.sign++;
 			else
 				return (false);
 		}
@@ -43,9 +51,8 @@ bool	is_num(char *arg)
 			count.int_num++;
 		i++;
 	}
-	if (count.int_num == 0 || count.dot > 1 || count.sign > 1)
-		return (false);
-	return (true);
+	return (count.int_num != 0 && count.dot <= 1 && \
+			count.sign <= 1 && ft_isdigit(arg[--i]));
 }
 
 size_t	parse_integer_part(const char *str, t_julia_parse *parse)
@@ -83,20 +90,4 @@ void	parse_decimal_part(const char *str, t_julia_parse *parse)
 		digit *= 10;
 		i++;
 	}
-}
-
-double	ft_atof(const char *str)
-{
-	t_julia_parse	parse;
-	size_t			i;
-
-	parse.double_num = 0.0;
-	parse.dot = 0;
-	parse.sign = 1;
-	i = 0;
-	i = parse_integer_part(str, &parse);
-	if (str[i] == '.')
-		i++;
-	parse_decimal_part(&str[i], &parse);
-	return (parse.sign * parse.double_num);
 }
